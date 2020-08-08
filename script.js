3fa8f61b5485a8490af1b65500f45167047387cc
KunZizinho <kun.zizinho@gmail.com>
2020-08-08 02:37:50 -0400

    correcting script

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var generateBtn = document.querySelector("#generate");
 
     return randElement
   }
+
 function writePassword() {
   document.querySelector("#password")
   var password = generatePassword();
@@ -37,54 +38,21 @@ function generatePasswordOptions() {
 
   if (length < 8 || length > 128) {
     alert("Chose a number between 8 and 128.");
+    return generatePasswordOptions();
+  } else {
+    console.log("here");
+    var numbers = confirm("Do you want numbers in your password");
+  
+    var lowerCases = confirm("Do you want lowercases in your password?");
+  
+  
+    var upperCases = confirm("Do you want uppercases in your password?");
+  
+  
+    var special = confirm("Do you want special characters in your password?");
+  };
 
-  }
-  console.log("here");
-  var numbers = confirm("Do you want numbers in your password");
- /* if (numbers === false) {
-    for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-      var randomnumber = Math.floor(Math.random() * valuesNoNumbers.length);
 
-      randomPasswordGenerated += valuesNoNumbers[randomnumber];
-
-    }
-  } */
-
-
-  var lowerCases = confirm("Do you want lowercases in your password?");
- // if (lowerCases === false) {
-  //   for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-  //     var randomnumber = Math.floor(Math.random() * valuesNoLower.length);
-
-  //     randomPasswordGenerated += valuesNoLower[randomnumber];
-
-  //   }
-  // }
-
-  var upperCases = confirm("Do you want uppercases in your password?");
-  // if (upperCases === false) {
-  //   for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-  //     var randomnumber = Math.floor(Math.random() * valuesNoUpper.length);
-
-  //     randomPasswordGenerated += valuesNoUpper[randomnumber];
-
-  //   }
-  // }
-
-  var special = confirm("Do you want special characters in your password?");
-  // if (special === false) {
-  //   for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-  //     var randomnumber = Math.floor(Math.random() * valuesNoSpecial.length);
-
-  //     randomPasswordGenerated += valuesNoSpecial[randomnumber];
-
-  //   }
-  // }
-
-  // if (numbers === false && lowerCases === false && upperCases === false && special === false) {
-  //   alert("you need to pick atleast one option")
-
-  // }
   if(
     numbers === false &&
     upperCases === false &&
@@ -104,39 +72,12 @@ function generatePasswordOptions() {
   };
 
 
-  // var minimumCount = 0;
 
-  // var minimumNumbers = "";
-  // var minimumLowerCases = "";
-  // var minimumUpperCases = "";
-  // var minimumSpecialCharacters = "";
+  //   GENERATPASSWORD FUNKCIJA KOJU SMO POVEZALI TAKO DA NA PRITISKOM NA GUMB ISKACE NAM POPUP PROZOR KOJI NAS PITA KOLIKO I KOJE ZNAKOVE ZELIMO KORISTITI I KOLIKO CE NAM DUGA SIFRA BITI
 
-
-  // //
-
-  // // loop getting random characters  FOR LOOP KOJIM NASUMICNO IZABIREMO ZNAKOVE
-  // for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-  //   console.log(length);
-  //   console.log(randomPasswordGenerated);
-  //   var randomnumber = Math.floor(Math.random() * values.length);
-
-  //   randomPasswordGenerated += values[randomnumber];
-
-
-  //   // to make sure characters are added to the password
-  //   randomPasswordGenerated += minimumNumbers;
-  //   randomPasswordGenerated += minimumLowerCases;
-  //   randomPasswordGenerated += minimumUpperCases;
-  //   randomPasswordGenerated += minimumSpecialCharacters;
-
-
-  //   return randomPasswordGenerated;
-
-  //   //   GENERATPASSWORD FUNKCIJA KOJU SMO POVEZALI TAKO DA NA PRITISKOM NA GUMB ISKACE NAM POPUP PROZOR KOJI NAS PITA KOLIKO I KOJE ZNAKOVE ZELIMO KORISTITI I KOLIKO CE NAM DUGA SIFRA BITI
-
-  // }
  return passwordOptions;
 }
+
 function generatePassword(){
   var options = generatePasswordOptions();
   console.log("options", options);
